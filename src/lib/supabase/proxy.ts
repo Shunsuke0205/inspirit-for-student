6ffd13de1f3088c1a91c65efe693ac1a6f8c3484
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2026-01-25 16:13:16 +0900

    bugfix: type error

--- src/lib/supabase/proxy.ts
+++ src/lib/supabase/proxy.ts
@@ -1,4 +1,4 @@
-import { createServerClient } from '@supabase/ssr'
+import { CookieMethodsServer, CookieOptions, createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function updateSession(request: NextRequest) {
@@ -6,27 +6,27 @@ export async function updateSession(request: NextRequest) {
     request,
   });
 
+  const cookieMethods: CookieMethodsServer = {
+    getAll() {
+      return request.cookies.getAll()
+    },
+    setAll(cookiesToSet: { name: string; value: string; options: CookieOptions }[]) {
+      cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
+      supabaseResponse = NextResponse.next({
+        request,
+      })
+      cookiesToSet.forEach(({ name, value, options }) =>
+        supabaseResponse.cookies.set(name, value, options)
+      )
+    },
+  };
+
   // With Fluid compute, don't put this client in a global environment variable.
   // Always create a new one on each request.
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return request.cookies.getAll()
-        },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
-          supabaseResponse = NextResponse.next({
-            request,
-          })
-          cookiesToSet.forEach(({ name, value, options }) =>
-            supabaseResponse.cookies.set(name, value, options)
-          )
-        },
-      },
-    }
+    { cookies: cookieMethods}
   );
 
   // Do not run code between createServerClient and supabase.auth.getClaims().
