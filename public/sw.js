12e49636ca6af81e8bb81c9794ea1c0263e44df9
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2026-02-01 13:50:37 +0900

    fix: test to set the notification count to 20

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@ self.addEventListener('push', function(event) {
   const count = data.count || 1;
 
   if (navigator.setAppBadge) {
-    navigator.setAppBadge().catch((error) => {
+    navigator.setAppBadge(20).catch((error) => {
       console.error('Badge update failed:', error);
     });
   }
