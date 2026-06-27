2ac02c8be546c0e4a3fe6db2aa0ffb851b97f1f0
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2026-06-27 23:51:34 +0900

    fix: argument 'canSubmit'

--- src/app/signup/SignupButton.tsx
+++ src/app/signup/SignupButton.tsx
@@ -2,20 +2,18 @@
 
 import { useFormStatus } from "react-dom";
 
-export const SignupButton = () => {
+export const SignupButton = ({
+  canSubmit = false
+} : { canSubmit?: boolean }) => {
   const { pending } = useFormStatus();
 
   return (
-    <div>
-      <button
-        disabled={pending}
-        type="submit"
-        className="
-          w-full py-3 px-4 bg-indigo-600 text-white font-semibold rounded-lg shadow-md hover:bg-indigo-700 transition duration-300 ease-in-out
-          disabled:cursor-not-allowed disabled:bg-indigo-300"
-      >
-        {pending ? "すこしお待ちになってください..." : "新規登録"}
-      </button>
-    </div>
+    <button
+      disabled={!canSubmit || pending}
+      type="submit"
+      className="w-full py-3 px-4 bg-indigo-600 text-white font-semibold rounded-lg shadow-md hover:bg-indigo-700 transition duration-300 ease-in-out disabled:cursor-not-allowed disabled:bg-indigo-300"
+    >
+      {pending ? "すこしお待ちになってください..." : "新規登録"}
+    </button>
   );
-}
+};
